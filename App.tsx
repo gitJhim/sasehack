3f78e9f6dad4d6519eccbf29fd05a3f22e6aa77d
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-10-04 16:04:54 -0400

    setting up gmaps

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
