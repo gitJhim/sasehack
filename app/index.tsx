9a73b0cd70f91567466e04800ea5e6e9a65d5724
ImanM9 <imanmotlagh17@gmail.com>
2024-10-05 01:37:55 -0400

    TopBar

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import TopBar from "../components/TopBar";
 
 const Index = () => {
   return (
     <View>
-      <Text className="text-blue-500">Hello World</Text>
+      <TopBar userLevel={100} userProfile={require("../assets/dummyProfile.png")}></TopBar>
+      <Text className="text-blue-500">Hello World!</Text>
     </View>
   );
 };
