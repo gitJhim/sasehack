26edfd18c19b5385eba8420f3ef818a4cc213504
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-10-05 22:53:32 -0400

    added add bin modal, SB CHECK  TO SEE IF THE BUTTON WORKS

--- components/AddBinModal.tsx
+++ components/AddBinModal.tsx
@@ -1,33 +1,72 @@
 import { useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import  Modal from 'react-native-modal';
+import { useMapStore } from "../state/stores/mapStore";
+import { useUserStore } from "../state/stores/userStore";
 
-export default function AddBinModal({isVisible}: {isVisible: boolean}) {
-  const [modalVisible, setModalVisible] = useState(isVisible);
-
+export default function AddBinModal({
+  isVisible,
+  setModalVisible,
+  latitude,
+  longitude,
+}: {
+  isVisible: boolean;
+  setModalVisible: (visible: boolean) => void;
+  latitude: number;
+  longitude: number;
+}) {
+  const [markerName, setMarkerName] = useState('');
+  const [fullness, setFullness] = useState('empty');
+  const user = useUserStore((state) => state.user);
+  const options = ["Empty", "Half full", "Full"];
   const addMarker = () => {
-
+    if (!user) {
+      return;
+    }
+    const newMarker = {
+      id: null,
+      title: markerName,
+      userId: user.id,
+      latitude: latitude,
+      longitude: longitude,
+    };
+    useMapStore.getState().addMarker(newMarker);
     setModalVisible(false);
   };
 
   return (
     <Modal
-      isVisible={modalVisible}
+      isVisible={isVisible}
       onBackdropPress={() => setModalVisible(false)}
     >
-      <View className="p-4 justify-center items-center">
-        <View className="flex-col justify-stretch items-center bg-white rounded-[25] w-6/12 py-8 px-2">
+      <View className="p-4 w-full flex-col bg-white rounded-[25] py-8 px-5 h-4/5 justify-evenly">
+        <View>
+        <Text className='text-black font-semibold text-2xl pb-2'>New Bin Name</Text>
+        <TextInput className="bg-gray-300 rounded-lg p-2 w-full mb-5" placeholder="Marker Name" onChangeText={(text) => setMarkerName(text)} />
+        </View>
+        <View>
+          <Text className='text-black font-semibold text-2xl pb-2'>How full is it?</Text>
+          {options.map((option) => (
+            <TouchableOpacity 
+              onPress={() => setFullness(option)}
+              className={`${option === fullness ? 'bg-[#17A773] text-white' : 'bg-gray-300 text-black'} rounded-lg p-2 w-full mb-5`}
+            >
+              <Text className="text-black font-semibold text-lg">{option}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View className="flex flex-row justify-evenly items-center">
           <TouchableOpacity 
             onPress={addMarker}
-            className="bg-[#17A773] py-2 px-4 rounded-full mb-5"
+            className="bg-[#17A773] py-4 px-8 rounded-full"
           >
-            <Text className="text-white font-semibold">Add Bin</Text>
+            <Text className="text-white font-semibold text-xl">Create</Text>
           </TouchableOpacity>
           <TouchableOpacity 
             onPress={() => setModalVisible(false)}
-            className="bg-[#17A773] py-2 px-4 rounded-full"
+            className="bg-gray-300 py-4 px-8 rounded-full"
           >
-            <Text className="text-white font-semibold">Add Cycle</Text>
+            <Text className="text-white font-semibold text-xl">Close</Text>
           </TouchableOpacity>
         </View>
       </View>
--- components/Map.tsx
+++ components/Map.tsx
@@ -15,6 +15,7 @@ import { useUserStore } from "../state/stores/userStore";
 import SignInModal from "./SignInModal";
 import { addNewMarker, getMarkers } from "../utils/db/map";
 import AddCycleModal from "./AddCycleModal";
+import AddBinModal from "./AddBinModal";
 
 export default function Map() {
   const markers = useMapStore((state) => state.markers);
@@ -36,6 +37,7 @@ export default function Map() {
   const [modalVisible, setModalVisible] = useState(false);
   const [signInModalVisible, setSignInModalVisible] = useState(false);
   const [recycleModalVisible, setRecycleModalVisible] = useState(false);
+  const [addBinModalVisible, setAddBinModalVisible] = useState(false);
 
   const getCurrentLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -79,15 +81,7 @@ export default function Map() {
       setSignInModalVisible(true);
       return;
     }
-
-    let newMarker = {
-      id: null,
-      userId: user.id,
-      latitude: myLocation.latitude,
-      longitude: myLocation.longitude,
-    };
-
-    await addNewMarker(newMarker);
+    setAddBinModalVisible(true);
   };
 
   const onAddRecycle = async () => {
@@ -136,6 +130,12 @@ export default function Map() {
         isVisible={recycleModalVisible}
         setModalVisible={setRecycleModalVisible}
       />
+      <AddBinModal
+        isVisible={addBinModalVisible}
+        setModalVisible={setAddBinModalVisible}
+        latitude={myLocation.latitude}
+        longitude={myLocation.longitude}
+      />
       <Modal
         isVisible={modalVisible}
         onBackdropPress={() => setModalVisible(false)}
@@ -167,10 +167,10 @@ export default function Map() {
         <Marker coordinate={myLocation} title="My Location" />
         {renderMarkers()}
       </MapView>
-      <TouchableOpacity onPress={goToCurrentLocation} className="bg-gray-300 rounded-3xl p-4 absolute bottom-[25%] right-5">
+      <TouchableOpacity onPress={goToCurrentLocation} style={styles.relocateButton}>
           <Image source={require('../assets/self.png')} style={{ width: 40, height: 40 }} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => setModalVisible(true)} className="bg-[#17A773] p-4 rounded-3xl absolute bottom-[15%] right-5">
+      <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.addButton}>
           <Image source={require('../assets/add_2.png')} style={{ width: 40, height: 40 }} />
       </TouchableOpacity>
     </View>
@@ -187,5 +187,21 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: Dimensions.get('window').height * 0.15,
+    right: Dimensions.get('window').width * 0.05,
+    backgroundColor: '#17A773',
+    padding: 16,
+    borderRadius: 24,
+  },
+  relocateButton: {
+    position: 'absolute',
+    bottom: Dimensions.get('window').height * 0.25,
+    right: Dimensions.get('window').width * 0.05,
+    backgroundColor: '#e2e8f0',
+    padding: 16,
+    borderRadius: 24,
   },
 });
