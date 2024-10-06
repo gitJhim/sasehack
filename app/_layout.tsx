711170c8f763a67532af8afab5f046b158d9bab2
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-10-06 03:13:55 -0400

    did nothing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,6 +34,7 @@ export default function AppLayout() {
       <ErrorBoundary>
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="profile" options={{ headerShown: false }} />
         </Stack>
       </ErrorBoundary>
     </>
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,10 +1,12 @@
-import {View, Image, Text, TextInput} from 'react-native';
+import {View, Image, Text, TextInput, Pressable} from 'react-native';
 import {UserStore} from '../types/user.types'
 import { useUserStore } from '../state/stores/userStore';
 import { useMapStore } from '../state/stores/mapStore';
+import { useRouter } from 'expo-router';
 
 export default function TopBar() {
     const user = useUserStore((state) => state.user);
+    const router = useRouter();
     
     if (!user) return(
         <View className="flex-row items-center h-36 bg-[#17A773] justify-evenly">
@@ -30,9 +32,9 @@ export default function TopBar() {
                     <Image source={require("../assets/search_check.png")} alt="search" width={20} height={20}/>
                     <TextInput className="mr-9" placeholder="Search" />
                 </View>
-                <View className="flex-row items-center mx-auto">
+                <Pressable onPress={() => router.push("/profile")} className="flex-row items-center mx-auto">
                     <Image source={user.avatarURL ? { uri: user.avatarURL } : require("../assets/dummyProfile.png")} alt="profile" width={40} height={40}/>
-                </View>
+                </Pressable>
                 <View className="flex-row p-2 h-12 w-40 rounded-full items-center bg-[#B1ECC8]">
                     <Image source={require("../assets/kid_star.png")} alt="star" width={20} height={20}/>
                     <Text className="font-bold items-center text-lg"> Level: {user.level}</Text>
